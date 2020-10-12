bf15fb8b2f6baa6e96882dbaf5a04e7c39dba8af
Storn White <stornwhite@Storns-Awesome-Computer-Box.local>
2020-10-12 14:38:57 -0700

    [PDJ-6][PJD-7] Initial FrameDef and ColumnDef objects

---
+++ objects/exceptions.py
@@ -0,0 +1,32 @@
+class PandjasErrorMixin(Exception):
+    """
+    Mixin for providing all exceptions with default messages and codes
+    """
+    def __init__(self, message=None):
+        self.message = message if message else self.default_detail
+
+    def __str__(self):
+        return self.message
+
+    @property
+    def default_detail(self):
+        raise NotImplementedError()
+
+    @property
+    def default_code(self):
+        raise NotImplementedError()
+
+
+class NoColumnDefNameError(PandjasErrorMixin, Exception):
+    default_detail = "All columns definitions must include a name."
+    default_code = "objects_no_column_def_name"
+
+
+class NoColumnDefFileDefError(PandjasErrorMixin, Exception):
+    default_detail = "All columns definitions must include a file_def."
+    default_code = "objects_no_column_def_file_def"
+
+
+class IllegalDtypeStringError(PandjasErrorMixin, Exception):
+    default_detail = "Incorrect string for identifying column dtype"
+    default_code = "objects_illegal_dtypes_string"
