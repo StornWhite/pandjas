6a062af64d7c0426522388fbdd3f053b8c93bae0
Storn White <stornwhite@Storns-Awesome-Computer-Box.local>
2020-10-15 10:53:30 -0700

    [PDJ-4] PDFrame Object

--- objects/objects.py
+++ objects/objects.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 import objects.exceptions as exc
@@ -14,9 +15,62 @@ class FrameDef(object):
     def column_names(self):
         return set(self.column_defs.keys())
 
+    @property
+    def empty_dataframe(self):
+        """
+        Returns an empty dataframe that conforms with the frame definition.
+        The dataframe will have no rows, but will have correctly named and
+        typed columns
+
+        :return: pandas dataframe
+        """
+        df = pd.DataFrame()
+        for key, value in self.column_defs.items():
+            df[key] = pd.Series(dtype=value.dtype)
+
+        return df
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~
+    # Backend methods
+    # ~~~~~~~~~~~~~~~~~~~~~~~
+
     def __init__(self):
 
         self.column_defs = dict()
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~
+    # User methods
+    # ~~~~~~~~~~~~~~~~~~~~~~~
+
+    def validate(self, dataframe):
+        """
+        Returns true if dataframe conforms to the frame definition.  To
+        conform, the dataframe must have *only* those columns defined
+        by the FrameDef, and those columns must match in name and dtype.
+
+        :param dataframe:
+        :return: boolean dataframe conforms
+        """
+        column_defs = self.column_defs.values()
+
+        if len(column_defs) != len(dataframe.columns):
+            # Missing or extra columns.
+            return False
+
+        for column_def in column_defs:
+
+            try:
+                column = dataframe[column_def.name]
+            except KeyError:
+                # Column is missing
+                return False
+
+            if column.dtype != column_def.dtype:
+                # Column has wrong type
+                return False
+
+        return True
+
 
     def create_column_def(self, name, dtype_str, is_input=True):
         """
@@ -79,3 +133,51 @@ class ColumnDef(object):
             raise exc.IllegalDtypeStringError()
 
         self.is_input = is_input
+
+
+class PDFrame(object):
+    """
+    The base-level wrapper for a pandas dataframe.  It includes both a
+    dataframe and a FrameDef object which can be used to validate that
+    the dataframe conforms to our data expectations.  It also includes
+    base-level methods for accessing a dataframe.
+
+    You could subclass PDFrame to create your own wrapper class with an
+    explicit frame definition and your own analysis methods for performing
+    high level operations and analysis on the dataframe with a goal of
+    populating result column data from input column data.
+    """
+    @property
+    def dataframe(self):
+        if hasattr(self, '_dataframe'):
+            return self._dataframe
+        else:
+            return None
+
+    @dataframe.setter
+    def dataframe(self, dataframe):
+        if self.frame_def.validate(dataframe):
+            self._dataframe = dataframe
+        else:
+            raise exc.InvalidDataFrameError
+
+    def __init__(self, frame_def=None, dataframe=None):
+        """
+        Sets or creates the PDFrame objects frame definition and initial
+        dataframe.
+
+        :param frame_def: FrameDef object
+        :param dataframe: panadas dataframe object
+        """
+        if frame_def is None:
+            self.frame_def = FrameDef()
+        else:
+            self.frame_def = frame_def
+
+        if dataframe is None:
+            # Todo: actually create a conforming but empty dataframe
+            self.dataframe = self.frame_def.empty_dataframe
+        else:
+            self.dataframe = dataframe
+
+
---
+++ objects/test/test_pd_frame.py
@@ -0,0 +1,79 @@
+from unittest import TestCase
+
+import pandas as pd
+
+from objects.objects import FrameDef, PDFrame
+
+
+class PDFrameTestCase(TestCase):
+
+    def setUp(self):
+
+        # Create FrameDef object
+        frame_def = FrameDef()
+        frame_def.create_column_def(
+            "timestamp",
+            "datetime64[ns, US/Pacific]"
+        )
+        frame_def.create_column_def(
+            "power",
+            "float"
+        )
+        frame_def.create_column_def(
+            "customer_id",
+            "UInt64"
+        )
+        frame_def.create_column_def(
+            "energy",
+            "float",
+            is_input=False
+        )
+
+        # Create empty dataframe
+        dataframe = frame_def.empty_dataframe
+
+        # Create PDFrame object
+        self.pd_frame = PDFrame(frame_def, dataframe)
+
+    def test_validate_valid(self):
+
+        # Initial dataframe should be valid.
+        pd_frame = self.pd_frame
+        self.assertTrue(
+            pd_frame.frame_def.validate(pd_frame.dataframe)
+        )
+
+    def test_remove_column_def(self):
+        """
+        Removing a column_def from the frame_def should result in a
+        valid dataframe becomeing invalid.
+        """
+        # Initial dataframe should be valid.
+        pd_frame = self.pd_frame
+        self.assertTrue(
+            pd_frame.frame_def.validate(pd_frame.dataframe)
+        )
+        # Now remove a ColumnDef from the FrameDef
+        pd_frame.frame_def.remove_column_def('power')
+        self.assertFalse(
+            pd_frame.frame_def.validate(pd_frame.dataframe)
+        )
+
+    def test_validate_invalid_columns(self):
+
+        # Drop a required column
+        pd_frame = self.pd_frame
+        pd_frame.dataframe.drop('power', axis=1, inplace=True)
+        self.assertFalse(
+            pd_frame.frame_def.validate(pd_frame.dataframe)
+        )
+
+    def test_validate_invalid_dtype(self):
+
+        # Change a required dtype
+        pd_frame = self.pd_frame
+        pd_frame.dataframe.drop('power', axis=1)
+        pd_frame.dataframe['power'] = pd.Series(dtype='UInt64').dtype
+        self.assertFalse(
+            pd_frame.frame_def.validate(pd_frame.dataframe)
+        )
